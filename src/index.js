d0b40a54229145cc6e310f8aa97720026696b188
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-01-22 06:58:27 +0530

    Added custom error classes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
 const { PORT } = require("./config/serverConfig");
+const apiRoutes = require("./routes/index");
+const db = require("./models/index");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use("/api", apiRoutes);
 async function startServer() {
   app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
   });
+  if (process.env.DB_SYNC) {
+    db.sequelize.sync({ alert: true });
+  }
 }
 startServer();
